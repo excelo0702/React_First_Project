45613bfcfceaa1a889d25d331ad091229830f14f
Tanish <tanishy0702@gmail.com>
2020-10-04 03:28:22 +0530

    Updates

---
+++ precache-manifest.eb54d0410c28f65facfb5c0b88a431e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd38baca0abce98807aaa104d030ff2",
+    "url": "/React_First_Project/index.html"
+  },
+  {
+    "revision": "9989800c8ef862072fae",
+    "url": "/React_First_Project/static/css/main.9ff06a69.chunk.css"
+  },
+  {
+    "revision": "8e892f14f3fd9f7e95f6",
+    "url": "/React_First_Project/static/js/2.d8657454.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_First_Project/static/js/2.d8657454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9989800c8ef862072fae",
+    "url": "/React_First_Project/static/js/main.891cd255.chunk.js"
+  },
+  {
+    "revision": "2903f85a665f042e91c4",
+    "url": "/React_First_Project/static/js/runtime-main.96c724a0.js"
+  }
+]);
